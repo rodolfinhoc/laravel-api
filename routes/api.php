3fdd6de2d22a1775647b33b931b7b3e777eb92cc
rodolfo-acti <53875025+rodolfinhoc@users.noreply.github.com>
2023-05-19 16:51:06 -0300

    atualização

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resource('tb_cad_usuario', 'UsuarioController');
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
